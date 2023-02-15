19951d5402554c01268f592a19b47a167e9b4cec
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-02-15 17:50:23 -0500

    footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,12 +8,21 @@ function Footer(){
     
    <div className='container-fluid bg-dark text-light py-5'>
         <div className='row'>
-            <div className='col-md-6 p-5'>
+            <div className='col-md-6 ps-5'>
                 <img 
                 className='mb-4'
                 src={Logo} 
                 />
                 <h3 className='heading_txt'>Take  student experience <br/>to the next level </h3>
+
+                <ul className='social_icons'>
+                <li><i className='fa fa-facebook'></i></li>
+                <li><i className='fa fa-youtube'></i></li>
+                <li><i className='fa fa-twitter'></i></li>
+                <li><i className='fa fa-instagram'></i></li>
+                <li><i className='fa fa-linkedin'></i></li>
+                <li><i className='fa fa-github'></i></li>
+                </ul>
             </div>
 
 
